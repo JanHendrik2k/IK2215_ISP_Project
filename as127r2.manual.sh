dfeca37c7aa3aa8597bf514e353bed6e04bb034f
Jan Hendrik Dahlhues <jd225735@fh-muenster.de>
2024-12-18 22:42:37 +0100

    project

---
+++ as127r2.manual.sh
@@ -0,0 +1,5 @@
+sudo chown student:student shared/as127r2/*
+cp -p shared/as127r2/zebra.conf as127r2/etc/quagga
+cp -p shared/as127r2/ospfd.conf as127r2/etc/quagga
+cp -p shared/as127r2/bgpd.conf as127r2/etc/quagga
+
