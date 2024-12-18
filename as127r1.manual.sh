2efb000bdd631146faa9526279d1fb50b0558c17
Jan Hendrik Dahlhues <jd225735@fh-muenster.de>
2024-12-18 22:51:22 +0100

    refactor

--- as127r1.manual.sh
+++
@@ -1,4 +0,0 @@
-sudo chown student:student shared/as127r1/*
-cp -p shared/as127r1/zebra.conf as127r1/etc/quagga
-cp -p shared/as127r1/ospfd.conf as127r1/etc/quagga
-cp -p shared/as127r1/bgpd.conf as127r1/etc/quagga
--- as127r2.manual.sh
+++
@@ -1,5 +0,0 @@
-sudo chown student:student shared/as127r2/*
-cp -p shared/as127r2/zebra.conf as127r2/etc/quagga
-cp -p shared/as127r2/ospfd.conf as127r2/etc/quagga
-cp -p shared/as127r2/bgpd.conf as127r2/etc/quagga
-
